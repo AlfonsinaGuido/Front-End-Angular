68cac673dd2e40ba4b0100c39153a897661d1ca2
Alfonsina Guido <aguido@aseinfo.com.sv>
2026-08-16 14:41:31 -0300

    Bloqueo el scroll mientras se cargan los datos.

--- src/app/components/datos-personales/datos-personales.component.ts
+++ src/app/components/datos-personales/datos-personales.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { finalize } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 import { DatosPersonales } from 'src/app/model/datos-personales';
 import { DatosPersonalesService } from 'src/app/services/datos-personales.service';
 import { TokenService } from 'src/app/services/token.service';
@@ -13,8 +13,13 @@ import { TokenService } from 'src/app/services/token.service';
 export class DatosPersonalesComponent implements OnInit {
   datos: DatosPersonales[] = [];
   isLoading = true;
+  private scrollBloqueado = false;
 
-  constructor(private DatosPersonales: DatosPersonalesService, private tokenService: TokenService) { }
+  constructor(
+    private DatosPersonales: DatosPersonalesService,
+    private tokenService: TokenService,
+    @Inject(DOCUMENT) private document: Document
+  ) { }
 
   isLogged = false;
   
@@ -31,10 +36,46 @@ export class DatosPersonalesComponent implements OnInit {
 
   cargarDatosPersonales(): void {
     this.isLoading = true;
-    this.DatosPersonales.lista()
-      .pipe(finalize(() => this.isLoading = false))
-      .subscribe(data => { this.datos = data });
+    this.bloquearScrollGlobal();
+    this.DatosPersonales.lista().subscribe(
+      data => {
+        this.datos = data;
+        this.isLoading = false;
+        this.desbloquearScrollGlobal();
+      },
+      err => {
+        console.error('No se pudieron cargar los datos', err);
+      }
+    );
   
+  }
+
+  private bloquearScrollGlobal(): void {
+    if (this.scrollBloqueado) {
+      return;
+    }
+
+    this.document.body.classList.add('loading-scroll-lock');
+    this.document.documentElement.classList.add('loading-scroll-lock');
+    this.scrollBloqueado = true;
+  }
+
+  private desbloquearScrollGlobal(): void {
+    if (!this.scrollBloqueado) {
+      return;
+    }
+
+    this.document.body.classList.remove('loading-scroll-lock');
+    this.document.documentElement.classList.remove('loading-scroll-lock');
+    this.scrollBloqueado = false;
+  }
+
+  @HostListener('document:keydown', ['$event'])
+  bloquearTecladoMientrasCarga(event: KeyboardEvent): void {
+    if (this.isLoading) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
   }
 
   delete(id?: number) {
@@ -53,4 +94,3 @@ export class DatosPersonalesComponent implements OnInit {
     }
   }
 }
-
